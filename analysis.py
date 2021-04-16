3f411f135ce37e8ec8dd2c8cc83879345bc4e20c
by-student-2017 <studentsctest@gmail.com>
2021-04-16 09:20:18 +0900

    windows 10

--- analysis.py
+++ analysis.py
@@ -2,14 +2,11 @@
 import dft_fit_csv_ads
 import dft_fit_csv_des
 
-mgs_DFA = __import__('BET-plot')
 mgs_PSD = __import__('PSD-plot_pygaps_v202')
 mgs_BET = __import__('BET-plot')
 mgs_Lan = __import__('Langmuir-plot')
-mgs_BET = __import__('BET-plot'
 mgs_tR  = __import__('t-plot_Ref_CarbonBlack')
 mgs_alp = __import__('alpha-s-plot')
 mgs_DH  = __import__('DH-plot')
 mgs_DR  = __import__('DR-plot')
 mgs_DA  = __import__('DA-plot')
-
