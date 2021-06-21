c9f2f384da0476eb2e4eb91a62d39efcf5d03fda
by-student-2017 <studentsctest@gmail.com>
2021-06-21 10:55:52 +0900

    windows 10

--- run.sh
+++ run.sh
@@ -41,6 +41,7 @@ echo "==========================================================================
 echo "alpha-s plot"
 echo " "
 #python3 alpha-s-plot.py
+python3 alpha-s-plot_CB_N2_77K_kaneko.py
 python3 alpha-s-plot_NGCB_N2_77K_Nakai.py
 python3 alpha-s-plot_GCB-I_N2_77K_Nakai.py
 #
