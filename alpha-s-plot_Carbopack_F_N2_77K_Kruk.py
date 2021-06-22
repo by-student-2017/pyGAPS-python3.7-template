1a19d63274c85ccef207d72b71bba8fb2243a6eb
by-student-2017 <studentsctest@gmail.com>
2021-06-22 11:09:51 +0900

    windows 10

--- alpha-s-plot_Carbopack_F_N2_77K_Kruk.py
+++ alpha-s-plot_Carbopack_F_N2_77K_Kruk.py
@@ -13,7 +13,7 @@ meso_end = 1.2
 ext_start = 1.5
 ext_end = 1.7
 
-as_coeff = 2.332581
+as_coeff = 6.2/2.658 # 2.332581
 
 def idx_of_the_nearest(data, value):
     idx = np.argmin(np.abs(np.array(data) - value))
